1179244564a209488158cf4443238d525133cb52
jopakka <jopakkamies@gmail.com>
2022-04-26 09:06:50 +0300

    removed socket from production

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import express from 'express';
 import db from './utils/db';
 import helmet from 'helmet';
 import {checkAuth} from './utils/auth';
-import {Server} from 'socket.io';
-import http from 'http';
 
 const httpPort = process.env.HTTP_PORT || 3000;
 
